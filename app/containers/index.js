4fe30bc1b595ab01ff5c3c8964b8f74315e628ba
Juphy <1571066810@qq.com>
2017-05-30 16:02:52 +0800

    m

--- app/containers/index.js
+++ app/containers/index.js
@@ -3,6 +3,10 @@
  */
 import React,{Component} from 'react';
 import RouterMap from '../routes/index'
+import {connect} from 'react-redux'
+//绑定actionCreator组成的对象
+import {bindActionCreators} from 'redux'
+import * as Actions from '../actions/userInfo'//获取所有actionCreator组成的对象
 export default class App extends Component{
     constructor(){
         super();
@@ -19,10 +23,24 @@ export default class App extends Component{
         )
     }
     componentDidMount(){
+        //现在本地查找是否存储过localStorage名字叫cityName
+        //1、没有就默认给一个
+        //2、将当前的城市存放在redux
         setTimeout(()=>{
             this.setState({
                 done:true
             })
         },500);
+
     }
 }
+export default connect(
+    state=>{//mapStateToProps
+        return{}
+    },
+    dispatch=>{//mapDispatchToProps
+        return{
+            UserActions:bindActionCreators(Actions,dispatch)
+        }
+    }
+)(App)
